1b71e1af507bd887207a8ad981b8ab510280bc49
Raphael Hagen <norlandrhagen@gmail.com>
2022-02-04 22:18:21 -0800

    watershed flow

--- flows_n_snows/watershed.py
+++ flows_n_snows/watershed.py
@@ -7,7 +7,7 @@ import xarray as xr
 
 
 def retrieve_dem_and_grid(
-    dem_path: Optional[str] = "../data/srtm1_30m_utah.tif",
+    dem_path: Optional[str] = "../data/srtm1_90m_utah.tif",
 ) -> tuple:
     """Retrieves grid and dem for pysheds watershed basin delineation.
     TODO: add subsetting based on bbox
@@ -15,7 +15,7 @@ def retrieve_dem_and_grid(
     Parameters
     ----------
     dem_path : Optional[str], optional
-        data path, by default '../data/srtm1_30m_utah.tif'
+        data path, by default '../data/srtm1_90m_utah.tif'
 
     Returns
     -------
@@ -49,6 +49,8 @@ def calculate_catchment_basin(grid, flowdir, pp_lat: float, pp_lon: float):
 def clip_catchment_to_grid(grid, catch):
     clipped_catch = grid.clip_to(catch)
     return clipped_catch
+
+
 
 
 # TODO:
